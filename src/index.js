5927d6c416870591cb533584a24b7633e9d6d157
redjay48 <rajdey4892@gmail.com>
2022-09-27 08:42:29 +0530

    Add starter files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import intro from "./tab1.js";
+import intro, {home} from "./tab1.js";
 
 intro();
+
+function addButton() {
+    const buttons = document.getElementById('buttons');
+
+    const tabOne = document.createElement('button');
+    const tabTwo = document.createElement('button');
+    const tabThree = document.createElement('button');
+
+    tabOne.textContent = 'Home';
+    tabTwo.textContent = 'Menu';
+    tabThree.textContent = 'Contact';
+
+    buttons.appendChild(tabOne);
+    buttons.appendChild(tabTwo);
+    buttons.appendChild(tabThree);
+
+}
+
+
+
+document.body.appendChild(addButton());
--- src/tab1.js
+++ src/tab1.js
@@ -1,4 +1,4 @@
-export default function intro () {
+export default function intro() {
     const heading = document.createElement('h1');
     const content = document.getElementById('content');
 
@@ -6,3 +6,32 @@ export default function intro () {
     content.appendChild(heading);
 
 }
+
+export function home() {
+    const main = document.getElementByTagName('main')
+    const homeDiv = document.createElement('div');
+    const divOne = document.createElement('div');
+    const divTwo = document.createElement('div');
+    homeDiv.classList.add('template');
+
+    const restOne = document.createElement('img');
+    // restOne.src = "img/restaurant1.jpg";
+    const descriptOne = document.createElement('p');
+    descriptOne.textContent = "Come to The Best Restaurant in the Bay Area."
+    const restTwo = document.createElement('img');
+    // restTwo.src = "img/restaurant2.jpg"
+    const descriptTwo = document.createElement('p');
+    descriptTwo.textContent = "Enjoy The Beach while you treat your tastebuds."
+
+
+   
+    
+    main.appendChild(homeDiv);
+    homeDiv.appendChild(divOne);
+    homeDiv.appendChild(divTwo);
+    divOne.appendChild(restOne);
+    divOne.appendChild(descriptOne);
+    divTwo.appendChild(restTwo);
+    divTwo.appendChild(descriptTwo);
+}
+
